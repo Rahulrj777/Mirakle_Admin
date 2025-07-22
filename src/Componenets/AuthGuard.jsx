4fc46921e955035ea75835d3975a20c34bce15e4
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-22 13:10:09 +0530

    Force redeploy with vercel.json update

--- src/Componenets/AuthGuard.jsx
+++ src/Componenets/AuthGuard.jsx
@@ -2,6 +2,7 @@ import { Navigate } from "react-router-dom"
 
 const AuthGuard = ({ children }) => {
   const token = localStorage.getItem("authToken")
+  window.location.href = "/login"; 
   if (!token) {
     return <Navigate to="/login" replace />
   }
