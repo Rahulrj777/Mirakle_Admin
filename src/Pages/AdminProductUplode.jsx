291886f271d5fdeb17b4287aa7de90563322f571
Rahul Srinivasan J <rahulsrinivasannkl@gmail.com>
2025-07-30 17:16:43 +0530

    Initial commit from new laptop

--- src/Pages/AdminProductUplode.jsx
+++ src/Pages/AdminProductUplode.jsx
@@ -837,7 +837,7 @@ export default function AdminProductUpload() {
                             </span>
                           </div>
                           <button
-                            onClick={() => toggleVariantStock(product._id, i, v.isOutOfStock)}
+                            onClick={() => toggleStock(product._id, i, v.isOutOfStock)}
                             className={`w-full text-xs px-3 py-2 rounded-lg font-medium transition-colors ${
                               v.isOutOfStock
                                 ? "bg-green-600 text-white hover:bg-green-700"
@@ -864,12 +864,12 @@ export default function AdminProductUpload() {
                       >
                         Delete
                       </button>
-                      <button
+                      {/* <button
                         onClick={() => toggleStock(product._id, product.isOutOfStock)}
                         className="w-full bg-blue-500 text-white px-4 py-2 rounded-xl hover:bg-blue-600 transition-colors font-medium"
                       >
                         {product.isOutOfStock ? "Set Product In Stock" : "Set Product Out of Stock"}
-                      </button>
+                      </button> */}
                     </div>
                   </div>
                 ))}
